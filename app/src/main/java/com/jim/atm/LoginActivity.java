f70980dcb7c205ed617e66066d18e2fc51f61432
jim82513 <jim82513@gmail.com>
2017-11-28 10:58:39 +0800

    登入畫面設計

--- app/src/main/java/com/jim/atm/LoginActivity.java
+++ app/src/main/java/com/jim/atm/LoginActivity.java
@@ -2,6 +2,8 @@ package com.jim.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login(View v){
+        EditText id = (EditText)findViewById(R.id.edituserid);
+        EditText pw = (EditText)findViewById(R.id.editpw);
+        String UserId = id.getText().toString();
+        String Userpw = pw.getText().toString();
+        
     }
 }
--- app/src/main/java/com/jim/atm/MainActivity.java
+++ app/src/main/java/com/jim/atm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jim.atm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +11,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    boolean login = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!login){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
