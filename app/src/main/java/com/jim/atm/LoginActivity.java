b12f9a05c2508262a7584d7ffccd3c471d93330c
jim82513 <jim82513@gmail.com>
2017-11-28 11:47:30 +0800

    Login

--- app/src/main/java/com/jim/atm/LoginActivity.java
+++ app/src/main/java/com/jim/atm/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.jim.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +19,13 @@ public class LoginActivity extends AppCompatActivity {
         EditText pw = (EditText)findViewById(R.id.editpw);
         String UserId = id.getText().toString();
         String Userpw = pw.getText().toString();
-        
+        if("jack".equals(UserId) && "123".equals(Userpw)){
+            Toast.makeText(this,"Success!",Toast.LENGTH_LONG).show();
+            finish();
+        }
+        else{
+            new AlertDialog.Builder(this).setMessage("SORRY!").setTitle("Atm").setPositiveButton("OK!",null).show();
+        }
+
     }
 }
